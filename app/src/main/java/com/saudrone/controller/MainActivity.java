b5413dbea5f9adfc88cbbdd8f973eddaaccdf75c
sinangilerol <erol.sinangil@gmail.com>
2020-06-03 04:27:56 +0300

    first activity serial works

--- app/src/main/java/com/saudrone/controller/MainActivity.java
+++ app/src/main/java/com/saudrone/controller/MainActivity.java
@@ -2,18 +2,35 @@ package com.saudrone.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.view.Window;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
 import com.saudrone.canvas.ArtificialHorizon;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static java.lang.Math.PI;
 import static java.lang.Math.atan2;
@@ -22,10 +39,89 @@ import static java.lang.Math.sqrt;
 public class MainActivity extends AppCompatActivity {
     ArtificialHorizon artificialHorizon;
     SeekBar armBar;
-    TextView textYaw,textPitch,textRoll,textGyroX,textGyroY,textGyroZ,textRCYaw,textRCPitch,textRCRoll,textRCThrottle;
-    float yaw=0.0f,pitch=0.0f,roll=0.0f;
+    TextView textYaw,textPitch,textRoll,textGyroX,textGyroY,textGyroZ,textRCYaw,textRCPitch,textRCRoll,textRCThrottle,textDataHertz,textLidar;
     SensorManager gyroManager, accManager;
     Sensor gyroSensor, accSensor;
+
+    //serial variables
+    public int count=0;
+    public int fark=0;
+    public String messageBuffer ="";
+
+
+    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+    UsbSerialPort port;
+    private SerialInputOutputManager mSerialIoManager;
+    SerialInputOutputManager usbIoManager;
+    private final SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+
+                @Override
+                public void onRunError(Exception e) {
+                    System.out.println("stop");
+                }
+
+                @Override
+                public void onNewData(final byte[] data) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            String appended = messageBuffer + new String(data, StandardCharsets.US_ASCII);
+                            if (appended.contains(".")){
+                                String[] parts = appended.split("\\.");
+                                if (parts.length == 1){ // parcanin sonunda extra paket yok
+                                    if (parts[0].length() == 20){ // uzunluk dogru ama ileride butaya pariti kontrol de eklenmeli
+                                        //log.append(parts[0]+" length:"+parts[0].length()+"\n");
+                                        if (count%20==0)
+                                        updateSerialDatas(parts[0]);
+
+
+                                        count++;
+                                        messageBuffer="";
+                                    }else{ // 20 dan buyukse bu paket okunmaz, buffer temizlenir
+                                        messageBuffer="";
+                                        Toast.makeText(MainActivity.this, "olmadiiiii", Toast.LENGTH_SHORT).show();
+                                    }
+                                }else{ //birden fazla parca varsa (ben 1. yi okudum, sonuncuyu buffera attım
+
+                                    String realmessage="";
+                                    for (int i=0;i<parts.length;i++){
+                                        if (parts[i].length() == 20 ){ //burda pariti kontrol de gelecek
+                                            realmessage=parts[i]; //for dongusunde oldugu icin en sonuncu duzgun datayı alacak
+                                            if (i == parts.length-1){
+                                                messageBuffer="";
+                                            }
+                                        }else { //paket 20 dan kucukse
+                                            if (i == parts.length-1){ // son parca ise buffera ekle
+                                                messageBuffer=parts[i];
+                                            }
+                                        }
+                                    }
+
+                                    if (!realmessage.isEmpty()){
+
+                                        //log.append(realmessage+" length:"+realmessage.length()+"\n");
+                                        if (count%20==0)
+                                        updateSerialDatas(parts[0]);
+                                        count++;
+                                    }
+                                }
+                            }else  { // paket tam gelmemis, buffera at
+                                messageBuffer=appended;
+                            }
+
+                            /*String s = new String(data, StandardCharsets.US_ASCII);
+                            log.append(s+" le"+data.length+"\n");
+                            count++;*/
+
+                        }
+                    });
+                }
+            };
+
+    public static final String INTENT_ACTION_GRANT_USB = BuildConfig.APPLICATION_ID + ".GRANT_USB";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -38,6 +134,20 @@ public class MainActivity extends AppCompatActivity {
         accManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accSensor = accManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         initViews();
+
+        baglan();
+        new CountDownTimer(Long.MAX_VALUE, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+                textDataHertz.setText(" Frekans:" + (count-fark)+" Hz");
+                fark=count;
+            }
+
+            public void onFinish() {
+
+            }
+        }.start();
 
 
         armBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -62,6 +172,61 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void baglanEvent(View view) {
+        usbIoManager.writeAsync(("aaaaaaaaa").getBytes());
+    }
+
+
+    private void baglan(){
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+
+        if (availableDrivers.isEmpty()) {
+            Toast.makeText(this, "cihaz yok", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+
+        if (connection == null) {
+            if (!manager.hasPermission(driver.getDevice())) {
+                PendingIntent usbPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(INTENT_ACTION_GRANT_USB), 0);
+                manager.requestPermission(driver.getDevice(), usbPermissionIntent);
+            }
+
+
+            Toast.makeText(this, "con yok", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+        try {
+            port.open(connection);
+            port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+            usbIoManager =new SerialInputOutputManager(port,mListener);
+            mExecutor.submit(usbIoManager);
+
+            Timer t=new Timer();
+            t.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    try{
+                        if (count > 10) {
+                            port.write(("0000000000000000.").getBytes(), 3);
+                        }
+                        //usbIoManager.writeAsync(("000000000000.").getBytes());
+                    }catch (Exception e){
+                    }
+                }
+            },0,5);
+            Toast.makeText(this, "basarili", Toast.LENGTH_SHORT).show();
+
+        }catch (Exception e){
+            Toast.makeText(this, "hataaa", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onResume() {
@@ -162,5 +327,26 @@ public class MainActivity extends AppCompatActivity {
         textRCPitch=findViewById(R.id.pitchRCTextView);
         textRCRoll=findViewById(R.id.rollRCTextView);
         textRCThrottle=findViewById(R.id.throttleRCTextView);
+
+        textDataHertz=findViewById(R.id.dataHertzTextView);
+
+        textLidar=findViewById(R.id.lidarTextView);
+    }
+
+    private void updateSerialDatas(String comingText){
+        if (comingText.length()!=20){
+            return;
+        }
+        String yaw=comingText.substring(0,4);
+        String pitch=comingText.substring(4,8);
+        String roll=comingText.substring(8,12);
+        String throttle=comingText.substring(12,16);
+        String lidar=comingText.substring(16,20);
+
+        textRCYaw.setText(yaw);
+        textRCPitch.setText(pitch);
+        textRCRoll.setText(roll);
+        textRCThrottle.setText(throttle);
+        textLidar.setText(lidar);
     }
 }
